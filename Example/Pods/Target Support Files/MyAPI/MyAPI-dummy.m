a6b073bf35dcdc6d34bdac4d650da1a0c3bb31db
孙丹龙 <sundanlong@gmail.com>
2017-06-26 22:51:42 +0800

    first commit

---
+++ Example/Pods/Target Support Files/MyAPI/MyAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_MyAPI : NSObject
+@end
+@implementation PodsDummy_MyAPI
+@end
---
+++ Example/Pods/Target Support Files/Pods-MyAPI_Tests/Pods-MyAPI_Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_MyAPI_Tests : NSObject
+@end
+@implementation PodsDummy_Pods_MyAPI_Tests
+@end
